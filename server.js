30f9adf25324fc573d5964d3da16c3b23c8cb188
wilkersoh <plgu@hotmail.com>
2019-04-18 16:00:23 +0800

    app

--- server.js
+++ server.js
@@ -5,13 +5,10 @@ const exphbs  = require('express-handlebars');
 const bodyparser = require("body-parser");
 const app = express();
 
-// app.set("view engine", "hbs");
-// app.engine
 app.set("views ", path.join(__dirname, './views/'));
-// app.engine("handlebars", exphbs({ defaultLayout: "mainLayout", layoutsDir: __dirname + "./views/layouts"}));
 app.engine('handlebars', exphbs({defaultLayout: 'mainLayout'}))
 app.set('view engine', 'handlebars');
-app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.urlencoded({extended: false}));
 
 // Route
 app.use('/employee', require("./controllers/employeeController"));
